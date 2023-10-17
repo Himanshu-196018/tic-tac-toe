5a64153ba4d55f251d05cd82af12690624d6c743
Himanshu <himanshu_196018@saitm.org>
2023-10-17 13:18:04 +0530

    created working game

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
+import App from "./components/App";
+import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello React with vite</h1>
+    <App />
   </React.StrictMode>
 );
